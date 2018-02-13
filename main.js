683c73baad8b4b4bb79a2f895d481db27414c652
tvondavi <tvondavi@andrew.cmu.edu>
2018-02-13 11:31:40 -0500

    todo fork is done

--- main.js
+++ main.js
@@ -3,5 +3,22 @@ function addNewList() {
 }
 
 function addListItem() {
-    console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var newItem = document.getElementById("new-list-item");
+    var itemInput = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item){
+	item.parentNode.removeChild(item);
+}
+
+function completeListItem(item){
+	if (item.checked){
+		item.parentNode.style.textDecoration = "line-through";
+	}
+	else{
+		item.parentNode.style.textDecoration = "none";
+	}
 }
